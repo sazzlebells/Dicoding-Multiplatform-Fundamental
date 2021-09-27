6279e1938dcb696a19f514e6d19c13abc45ce15d
Fradipta <65805640+fradip12@users.noreply.github.com>
2021-09-28 01:02:32 +0700

    search

---
+++ restaurant_api/lib/provider/search_provider.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+// import 'package:restaurant_api/common/enums.dart';
+// import 'package:restaurant_api/data/api/api_service.dart';
+// import 'package:restaurant_api/data/model/restaurant.dart';
+// import 'package:restaurant_api/provider/detail_provider.dart';
+
+// class SearchProvider extends ChangeNotifier {
+//   final RestaurantResult result;
+
+//   late RestaurantResult _restaurantResult;
+//   String _message = "";
+//   late ResultState _state;
+
+//   SearchProvider(this.result);
+
+//   Future search(String query) async {
+//     RestaurantResult _realData;
+//     RestaurantResult _searchResult;
+//     _realData = 
+//     print('search for');
+//     print(query);
+//     _realData = _restaurantResult;
+//     print(_realData);
+//   }
+// }

f247d5cdbca5a9507514fd9079b27021c2b06dc7
Winnona Sarah <62462670+sazzlebells@users.noreply.github.com>
2021-09-28 01:11:07 +0700

    add review ui

--- restaurant_api/lib/ui/detail_page.dart
+++ restaurant_api/lib/ui/detail_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_api/common/enums.dart';
+import 'package:restaurant_api/common/style.dart';
 import 'package:restaurant_api/data/api/api_service.dart';
+import 'package:restaurant_api/data/model/detail.dart';
 import 'package:restaurant_api/data/model/restaurant.dart';
+import 'package:restaurant_api/provider/detail_provider.dart';
+import 'package:restaurant_api/ui/review_section.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key, required this.restaurant}) : super(key: key);
@@ -12,39 +18,103 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("${restaurant.name}")),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Hero(
-              tag: restaurant.pictureId.toString(),
-              child: Image.network(
-                  _service.getPicture + "${restaurant.pictureId}"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Location: ${restaurant.city}',
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Rating: ${restaurant.rating}',
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  Divider(color: Colors.grey),
-                  Text(
-                    restaurant.description ?? "",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
-              ),
-            ),
-          ],
+      body: ChangeNotifierProvider<DetailProvider>(
+        create: (_) =>
+            DetailProvider(service: ApiService(), id: restaurant.id.toString()),
+        child: Consumer<DetailProvider>(
+          builder: (context, state, _) {
+            var _state = state.state;
+            var data = state.result!.restaurant;
+
+            if (_state == ResultState.Loading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (_state == ResultState.HasData) {
+              return _body(context, data);
+            } else if (_state == ResultState.NoData) {
+              return Center(child: Text(state.message));
+            } else {
+              return Center(child: Text(""));
+            }
+          },
         ),
       ),
     );
+  }
+
+  SingleChildScrollView _body(BuildContext context, Detail? data) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Hero(
+            tag: data!.pictureId.toString(),
+            child: Image.network(_service.getPicture + "${data.pictureId}"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Location: ${data.city}',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Rating: ${data.rating}',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Categories: ", style: myTextTheme.subtitle2),
+                        Wrap(spacing: 5, children: _chips(data.categories)),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15),
+                Text("Description", style: myTextTheme.headline5),
+                Divider(color: Colors.grey),
+                Text(
+                  data.description ?? "",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                SizedBox(height: 15),
+                Text("Menu", style: myTextTheme.headline5),
+                Divider(color: Colors.grey),
+                Table(),
+                SizedBox(height: 15),
+                SizedBox(height: 15),
+                Text("Reviews (${data.customerReviews!.length})",
+                    style: myTextTheme.headline5),
+                Divider(color: Colors.grey),
+              ],
+            ),
+          ),
+          ReviewSection(review: data),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _chips(List<Category>? cat) {
+    List<Widget> chip = [];
+    for (var i = 0; i < cat!.length; i++) {
+      chip.add(Chip(
+        label: Text(cat[i].name.toString()),
+        labelStyle: myTextTheme.overline,
+      ));
+    }
+
+    return chip;
   }
 }

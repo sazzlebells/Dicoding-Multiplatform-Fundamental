5958ea84724b48b981312f387d66b4f6184a89c6
Winnona Sarah <62462670+sazzlebells@users.noreply.github.com>
2021-09-29 00:23:56 +0700

    check connectivity

--- restaurant_api/lib/ui/home_page.dart
+++ restaurant_api/lib/ui/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_api/data/api/api_service.dart';
 import 'package:provider/provider.dart';
-import 'package:restaurant_api/data/model/restaurant.dart';
 import 'package:restaurant_api/provider/restaurant_provider.dart';
 import 'package:restaurant_api/ui/restaurant_list_page.dart';
+import 'package:cross_connectivity/cross_connectivity.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,47 +14,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Widget appBarTitle = Text("My Resto");
+  Widget appBarTitle = const Text("My Resto");
   IconData icon = Icons.search;
   TextEditingController controller = new TextEditingController();
-  bool? _isSearching;
-  String? _searchText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    Connectivity().checkConnection().then((value) {
+      print(value);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<RestaurantProvider>(
-        create: (_) => RestaurantProvider(service: ApiService()),
-        child: Scaffold(
-          appBar: AppBar(title: appBarTitle, actions: [
-            Consumer<RestaurantProvider>(
-              builder: (c, s, _) {
-                return IconButton(
-                  icon: Icon(icon),
-                  onPressed: () {
-                    setState(() {
-                      if (icon == Icons.search) {
-                        icon = Icons.search;
-                        appBarTitle = TextField(
-                          // controller = controller,
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.search),
-                            hintText: "Search...",
-                          ),
-                          onChanged: (value) {
-                            s.search(value);
-                            setState(() {
-                              controller.text = value;
-                            });
-                          },
-                        );
-                      }
-                    });
-                  },
-                );
-              },
-            )
-          ]),
-          body: const RestaurantList(),
-        ));
+    return ConnectivityBuilder(builder: (context, isConnected, status) {
+      return isConnected == true
+          ? ChangeNotifierProvider<RestaurantProvider>(
+              create: (_) =>
+                  RestaurantProvider(service: ApiService(), context: context),
+              child: Scaffold(
+                appBar: AppBar(title: appBarTitle, actions: [
+                  Consumer<RestaurantProvider>(
+                    builder: (c, s, _) {
+                      return IconButton(
+                        icon: Icon(icon),
+                        onPressed: () {
+                          setState(() {
+                            if (icon == Icons.search) {
+                              icon = Icons.search;
+                              appBarTitle = TextField(
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Icons.search),
+                                  hintText: "Search...",
+                                ),
+                                onChanged: (value) {
+                                  s.search(value);
+                                  setState(() {
+                                    controller.text = value;
+                                  });
+                                },
+                              );
+                            }
+                          });
+                        },
+                      );
+                    },
+                  )
+                ]),
+                body: const RestaurantList(),
+              ))
+          : const Scaffold(
+              body: Center(
+                child: Text("No Internet Connection"),
+              ),
+            );
+    });
   }
 }

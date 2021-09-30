74a048c049abe3cf873ad258fba2ab921b80f5a0
Winnona Sarah <62462670+sazzlebells@users.noreply.github.com>
2021-09-30 20:59:55 +0700

    add database helper & provider

---
+++ restaurant_api/lib/provider/database_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:restaurant_api/utils/enums.dart';
+import 'package:restaurant_api/data/db/database_helper.dart';
+import 'package:restaurant_api/data/model/restaurant.dart';
+
+class DatabaseProvider extends ChangeNotifier {
+  final DatabaseHelper dbHelper;
+
+  DatabaseProvider({required this.dbHelper}) {
+    _getFavorites();
+  }
+
+  late ResultState _state = ResultState.NoData;
+  ResultState get state => _state;
+
+  String _message = "Empty Data";
+  String get message => _message;
+
+  List<Restaurant> _favorites = [];
+  List<Restaurant> get favorites => _favorites;
+
+  void _getFavorites() async {
+    _favorites = await dbHelper.getFavorites();
+    if (_favorites.length > 0) {
+      _state = ResultState.HasData;
+    } else {
+      _state = ResultState.NoData;
+      _message = "Empty Data";
+    }
+    notifyListeners();
+  }
+
+  void addFavorite(Restaurant restaurant) async {
+    try {
+      await dbHelper.addFavorite(restaurant);
+      _getFavorites();
+    } catch (e) {
+      _state = ResultState.Error;
+      _message = "Error: $e";
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isFavorited(String id) async {
+    final favRestaurant = await dbHelper.getFavoriteById(id);
+    return favRestaurant.isNotEmpty;
+  }
+
+  void removeFavorite(String id) async {
+    try {
+      await dbHelper.removeFavorite(id);
+      _getFavorites();
+    } catch (e) {
+      _state = ResultState.Error;
+      _message = "Error: $e";
+      notifyListeners();
+    }
+  }
+}

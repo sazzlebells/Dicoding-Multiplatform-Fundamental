d054d89a2af4b33be4bb7edde9a38d5ab81897bc
Winnona Sarah <62462670+sazzlebells@users.noreply.github.com>
2021-10-04 10:19:54 +0700

    add bottom nav bar tu ui

--- restaurant_api/lib/ui/home_page.dart
+++ restaurant_api/lib/ui/home_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_api/data/api/api_service.dart';
 import 'package:provider/provider.dart';
-import 'package:restaurant_api/data/db/database_helper.dart';
-import 'package:restaurant_api/provider/database_provider.dart';
 import 'package:restaurant_api/provider/restaurant_provider.dart';
 import 'package:restaurant_api/ui/favorite_page.dart';
 import 'package:restaurant_api/ui/restaurant_list_page.dart';
 import 'package:cross_connectivity/cross_connectivity.dart';
+import 'package:restaurant_api/utils/enums.dart';
+import 'package:restaurant_api/widgets/bottom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,9 +23,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    Connectivity().checkConnection().then((value) {
-      print(value);
-    });
+    Connectivity().checkConnection();
   }
 
   @override
@@ -35,44 +32,48 @@ class _HomePageState extends State<HomePage> {
       return isConnected == true
           ? Scaffold(
               appBar: AppBar(title: appBarTitle, actions: [
-                Consumer<RestaurantProvider>(
-                  builder: (c, s, _) {
-                    return IconButton(
-                      icon: Icon(icon),
-                      onPressed: () {
-                        setState(() {
-                          if (icon == Icons.search) {
-                            icon = Icons.search;
-                            appBarTitle = TextField(
-                              decoration: const InputDecoration(
-                                prefixIcon: Icon(Icons.search),
-                                hintText: "Search...",
-                              ),
-                              onChanged: (value) {
-                                s.search(value);
-                                setState(() {
-                                  controller.text = value;
-                                });
-                              },
-                            );
-                          }
-                        });
-                      },
-                    );
-                  },
-                ),
+                _search(),
                 IconButton(
                     icon: const Icon(Icons.favorite),
                     onPressed: () =>
                         Navigator.pushNamed(context, FavoritePage.routeName)),
               ]),
               body: const RestaurantList(),
+              bottomNavigationBar:
+                  const BottomNavBar(selectedMenu: MenuState.home),
             )
           : const Scaffold(
-              body: Center(
-                child: Text("No Internet Connection"),
-              ),
+              body: Center(child: Text("No Internet Connection")),
             );
     });
+  }
+
+  Consumer<RestaurantProvider> _search() {
+    return Consumer<RestaurantProvider>(
+      builder: (c, s, _) {
+        return IconButton(
+          icon: Icon(icon),
+          onPressed: () {
+            setState(() {
+              if (icon == Icons.search) {
+                icon = Icons.search;
+                appBarTitle = TextField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Search...",
+                  ),
+                  onChanged: (value) {
+                    s.search(value);
+                    setState(() {
+                      controller.text = value;
+                    });
+                  },
+                );
+              }
+            });
+          },
+        );
+      },
+    );
   }
 }

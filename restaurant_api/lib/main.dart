0d0d7248a8abf60f1ac28e486b83bbcf2807eee2
Winnona Sarah <62462670+sazzlebells@users.noreply.github.com>
2021-10-04 10:20:24 +0700

    notif and settings page route

--- restaurant_api/lib/main.dart
+++ restaurant_api/lib/main.dart
@@ -1,19 +1,41 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_api/common/color.dart';
+import 'package:restaurant_api/common/navigation.dart';
 import 'package:restaurant_api/common/style.dart';
 import 'package:restaurant_api/data/api/api_service.dart';
 import 'package:restaurant_api/data/db/database_helper.dart';
 import 'package:restaurant_api/data/model/restaurant.dart';
+import 'package:restaurant_api/data/preferences/preferences_helper.dart';
 import 'package:restaurant_api/provider/database_provider.dart';
+import 'package:restaurant_api/provider/preferences_provider.dart';
 import 'package:restaurant_api/provider/restaurant_provider.dart';
+import 'package:restaurant_api/provider/scheduling_provider.dart';
 import 'package:restaurant_api/ui/detail_page.dart';
 import 'package:restaurant_api/ui/favorite_page.dart';
 import 'package:restaurant_api/ui/home_page.dart';
+import 'package:restaurant_api/ui/settings_page.dart';
+import 'package:restaurant_api/utils/background_service.dart';
+import 'package:restaurant_api/utils/notification_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate();
+
+  await AndroidAlarmManager.initialize();
+  await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+
   runApp(const MyApp());
 }
 
@@ -32,6 +54,13 @@ class MyApp extends StatelessWidget {
             create: (_) => DatabaseProvider(dbHelper: DatabaseHelper())),
         ChangeNotifierProvider<RestaurantProvider>(
           create: (_) => RestaurantProvider(service: ApiService()),
+        ),
+        ChangeNotifierProvider(create: (_) => SchedulingProvider()),
+        ChangeNotifierProvider(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+                sharedPreferences: SharedPreferences.getInstance()),
+          ),
         ),
       ],
       child: _materialApp(),
@@ -55,15 +84,15 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+      navigatorKey: navigatorKey,
       initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (context) => const HomePage(),
         DetailPage.routeName: (context) => DetailPage(
-              restaurant:
-                  ModalRoute.of(context)?.settings.arguments as Restaurant,
-              // isFavorite: ModalRoute.of(context)?.settings.arguments as bool,
-            ),
+            restaurant:
+                ModalRoute.of(context)?.settings.arguments as Restaurant),
         FavoritePage.routeName: (context) => const FavoritePage(),
+        SettingsPage.routeName: (context) => const SettingsPage(),
       },
     );
   }
